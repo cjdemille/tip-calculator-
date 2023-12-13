53911c9c0d1e49255c9eb5b6325248d7e167e049
cjdemille <cjdemille5@gmail.com>
2023-12-12 19:00:21 -0500

    add html and styles

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const totalDisplay = document.querySelector('#total-per-person');
+const tipDisplay = document.querySelector('#tip-per-person');
+
+
+totalDisplay.innerHTML = "32.79";
+tipDisplay.innerHTML = '4.27';
