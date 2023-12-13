ab4062290c58265e099e64193ca182f9efdc7aa8
cjdemille <cjdemille5@gmail.com>
2023-12-13 11:09:24 -0500

    add listeners for tip buttons

--- js/app.js
+++ js/app.js
@@ -5,52 +5,72 @@ const customTip = document.querySelector('#custom-tip');
 const numPeopleInput = document.querySelector('#num-people');
 
 const tenPercent = document.querySelector('#ten');
+const fifteenPercent = document.querySelector('#fifteen');
+const twentyPercent = document.querySelector('#twenty');
+const twentyFivePercent = document.querySelector('#twenty-five');
+const fiftyPercent = document.querySelector('#fifty');
 
-const billAmount = billInput.value;
-const numPeople = numPeopleInput.value;
-
-const calcTotal = (total , numPeople) => {
-    totalDisplay.innerHTML = Number(total) / Number(numPeople);
-}
-
-
-// totalDisplay.innerHTML = "32.79";
-// tipDisplay.innerHTML = '4.27';
-
-billInput.addEventListener('change', () =>{
-    if(Number(billInput.value) > 0 && Number(numPeopleInput.value) > 0 ){
-        calcTotal(billInput.value, numPeopleInput.value);
-    }
-})
-
-numPeopleInput.addEventListener('change', () =>{
-    if(Number(billInput.value) > 0 && Number(numPeopleInput.value) > 0 ){
-        calcTotal(billInput.value, numPeopleInput.value);
-    }
-})
-
-const getTipValue = (ele) =>{
-    for(i = 0; i < ele.length; i++){
-        if (ele[i].type="radio"){
-          if(ele[i].checked){
-            return Number(ele[i].value);
-          }
+const getTipValue = (ele) => {
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].type = "radio") {
+            if (ele[i].checked) {
+                console.log(ele[i].value)
+                return Number(ele[i].value);
+            }
         }
-      }
+    }
 }
 
 const calcTip = (tip) => {
-    const percentage = tip/100; 
+    const percentage = tip / 100;
     return Number(billInput.value) * percentage;
 }
 
 const runTip = () => {
-    const ele = document.getElementsByTagName('input');
+    const ele = document.getElementsByName('tip');
     const tipVal = getTipValue(ele);
     const tipAmount = calcTip(tipVal);
     tipDisplay.innerHTML = tipAmount;
 }
+const calculateTipAndTotal = () => {
+    const ele = document.getElementsByName('tip');
+    const tipVal = getTipValue(ele);
+    const tipAmount = calcTip(tipVal);
+    const tipPerPerson = tipAmount / Number(numPeopleInput.value);
+    const tipPerDisplay = tipPerPerson.toFixed(2);
+    tipDisplay.innerHTML = tipPerDisplay
+    const totalAmount = tipAmount + Number(billInput.value);
+    console.log(totalAmount);
+    const personalAmount = totalAmount / Number(numPeopleInput.value)
+    const personalDisplay = personalAmount.toFixed(2);
+    console.log(personalAmount);
+    totalDisplay.innerHTML = personalDisplay;
+}
+
+const runUpdate = () => {
+    if (Number(billInput.value) > 0 && Number(numPeopleInput.value) > 0) {
+        calculateTipAndTotal();
+    }
+}
 
 
-tenPercent.addEventListener('change', runTip);
+billInput.addEventListener('change', runUpdate);
 
+numPeopleInput.addEventListener('change', runUpdate);
+
+tenPercent.addEventListener('change', runUpdate);
+fifteenPercent.addEventListener('change', runUpdate);
+twentyPercent.addEventListener('change', runUpdate);
+twentyFivePercent.addEventListener('change', runUpdate);
+fiftyPercent.addEventListener('change', runUpdate);
+
+
+// To Dos
+
+// reset button
+// custom tip input 
+// make bill input populate like money 
+// validate that bill has two decimal places
+// validate that number of people is a whole number
+// fine tune styles for mobile
+// create desktop layout
